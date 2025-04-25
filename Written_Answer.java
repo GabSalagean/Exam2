28048f9c9291750cb86cf77d53b37fab6b7b1686
GabSalagean <gaby.slg@icloud.com>
2025-04-25 09:05:36 -0400

    Answers written on the exam

--- Written_Answer.java
+++ Written_Answer.java
@@ -31,3 +31,31 @@ public class Written_Answer
         return x + y;
     }
 }
+
+//*#24
+//1-get values
+//2-code & assign a string
+//3-code & assign a 2 string
+//4-if string 1 equal to string 2
+    // then ("equal");
+//5- if string 1 is equal to string 2
+//  then ("not equal");
+
+//*#25
+//1-import from java; Array list;
+//2-get values
+//3-if temperature greater than 37.5
+    //then system.out.println("fever");
+//if temperature less than 37.5
+    //then system.out.println ("no fever");
+//4-open codepad
+//5-view highest temp
+
+//*#26
+//after getting the values, I would still print out the temperatures on the code pad. but after that, I would
+//use the Collections.max in the temperature values instead of the loop.
+
+//*#27
+//After I would call the Array list, I would use the sort method & create a temperature object too. In the temp code, I would
+//call the array list & use the sort in there. There would not be a loop in this method neither
+//since there is no return value
